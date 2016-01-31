7983c802cf0d299536804fa0df10ad119f251a09
Raphaël Rochet <raphael@rri.fr>
2016-01-31 18:11:24 +0100

    GNU GPL v3 Licence addition

--- prefs.js
+++ prefs.js
@@ -1,3 +1,22 @@
+/*
+    This file is part of Arch Linux Updates Indicator
+
+    Arch Linux Updates Indicator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Arch Linux Updates Indicator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Arch Linux Updates Indicator.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2016 Raphaël Rochet
+*/
+
 const GObject = imports.gi.GObject;
 const Gtk = imports.gi.Gtk;
 const Gio = imports.gi.Gio;
