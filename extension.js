RaphaelRochet/arch-update

commit 30176c8d247fc3a9153c3eff4191a32d101a153c

Unlink directory monitor on destroy

extension.js
@@ -202,6 +202,13 @@ const ArchUpdateIndicator = new Lang.Class({
 	},
 
 	destroy: function() {
+		if (this.monitor && this.monitorId) {
+			// Stop spying on pacman local dir
+			this.monitor.disconnect(monitorId);
+			this.monitorId = null;
+			this.monitor.cancel();
+			this.monitor = null;
+		}
 		if (this._updateProcess_sourceId) {
 			// We leave the checkupdate process end by itself but undef handles to avoid zombies
 			GLib.source_remove(this._updateProcess_sourceId);
@@ -250,7 +257,7 @@ const ArchUpdateIndicator = new Lang.Class({
 		if (PACMAN_DIR) {
 			this.pacman_dir = Gio.file_new_for_path(PACMAN_DIR);
 			this.monitor = this.pacman_dir.monitor_directory(0, null);
-			this.monitor.connect('changed', Lang.bind(this, this._onFolderChanged));
+			this.monitorId = this.monitor.connect('changed', Lang.bind(this, this._onFolderChanged));
 		}
 	},
 	_onFolderChanged: function() {

commit 07ce51d729bef26d50e8a44bd6d20ccbedb7c4e1

Fix a bug introduced in 30176c8d247fc3a9153c3eff4191a32d101a153c

extension.js
@@ -202,10 +202,8 @@ const ArchUpdateIndicator = new Lang.Class({
 	},
 
 	destroy: function() {
-		if (this.monitor && this.monitorId) {
+		if (this.monitor) {
 			// Stop spying on pacman local dir
-			this.monitor.disconnect(monitorId);
-			this.monitorId = null;
 			this.monitor.cancel();
 			this.monitor = null;
 		}
@@ -257,7 +255,7 @@ const ArchUpdateIndicator = new Lang.Class({
 		if (PACMAN_DIR) {
 			this.pacman_dir = Gio.file_new_for_path(PACMAN_DIR);
 			this.monitor = this.pacman_dir.monitor_directory(0, null);
-			this.monitorId = this.monitor.connect('changed', Lang.bind(this, this._onFolderChanged));
+			this.monitor.connect('changed', Lang.bind(this, this._onFolderChanged));
 		}
 	},
 	_onFolderChanged: function() {