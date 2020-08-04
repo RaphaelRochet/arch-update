53bb809a95cd9b405886c164932c682fb646bf19
hlechner <hlechner@gmail.com>
2020-08-04 17:48:54 -0300

    add option to select the position of the indicator

--- prefs.js
+++ prefs.js
@@ -59,6 +59,8 @@ function buildPrefsWidget(){
 	settings.bind('pacman-dir' , buildable.get_object('field_pacmandir') , 'text' , Gio.SettingsBindFlags.DEFAULT);
 	settings.bind('auto-expand-list', buildable.get_object('field_autoexpandlist'), 'value', Gio.SettingsBindFlags.DEFAULT);
 	settings.bind('package-manager' , buildable.get_object('field_packagemanager') , 'text' , Gio.SettingsBindFlags.DEFAULT);
+	settings.bind('position' , buildable.get_object('field_position') , 'active' , Gio.SettingsBindFlags.DEFAULT);
+	settings.bind('position-number' , buildable.get_object('field_positionnumber') , 'value' , Gio.SettingsBindFlags.DEFAULT);
 
 	box.show_all();
 	return box;
